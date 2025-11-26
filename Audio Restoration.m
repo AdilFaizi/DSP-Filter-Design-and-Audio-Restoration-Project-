99b439296f5abd0263d8b90668db764912bbdaca
Adil Faizi <mohammadadilfaizi@gmail.com>
2025-11-26 15:14:07 -0500

    Refactor audio restoration script with time domain analysis

--- Audio Restoration.m
+++ Audio Restoration.m
@@ -2,7 +2,8 @@
 % by Adil Faizi, 220290664
 % By using frequency domain plot analysis, we can determine that two specific frequencies stand out in this sound signal. Then utilizing a notch filter to reduce their magnitudes
 
-
+%%--------------------------------------------------------------
+%Frequency Domain Equivalent
 [X, fs] = audioread("music_noisy.wav");
 
 % FFT of original
@@ -39,5 +40,51 @@ xlim([0 fs/2]);
 sound(filtered_X, fs);
 %write sound file
 audiowrite("filtered_noisysound.wav", filtered_X, fs);
+
+%%-------------------------------------------------------------
+%Time Domain Equivalent
+[X, fs] = audioread("music_noisy.wav");
+
+% Using time domain to plot the sound
+t = (0:length(X)-1) / fs; % Create a time vector based on the sampling frequency
+plot(t, X); % Plot the audio signal in the time domain
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Audio Signal in Time Domain');
+
+
+%midpoint frequency 
+f_center = 1102.49;   
+bw = 12; %this is our bandwith so i want it have a range of an upper and lower freq.
+f_lower = f_center - bw;   
+f_upper = f_center + bw;
+F = [0 f_lower f_upper fs/2]; 
+A = [1 0 1];                   
+Dev = [0.01  0.01 0.01 ];  
+[N, Fi, Ai, W] = firpmord(F, A, Dev, fs);  
+b = firpm(N, Fi, Ai, W);    
+figure;
+freqz(b, 1);
+title('1102.49 Hz FIR Filter');
+
+%same process as 1102.49 frequency.
+f_center2 = 2756.27;  
+bw2 = 12;             
+f_lower2 = f_center2 - bw2;  
+f_upper2 = f_center2 + bw2;  
+F2 = [0 f_lower2 f_upper2 fs/2];  
+A2 = [1 0 1];                      
+Dev2 = [0.01  0.01 0.01 ];    
+[N2, Fi2, Ai2, W2] = firpmord(F2, A2, Dev2, fs);  
+b2 = firpm(N2, Fi2, Ai2, W2);                    
+figure;
+freqz(b2, 1);
+title('2756.27 Hz FIR Filter');
+
+
+filtered_1 = filter(b, 1, X); %yq
+filtered_2 = filter(b2, 1, filtered_1);
+
+sound(Y2, fs);
 
 
